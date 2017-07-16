8a14b64e5f156d7b8f6d51fb16c5886505681418
LvYang <tylvyang@qq.com>
2017-07-17 01:18:52 +0800

    step 7 upload resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,29 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
-  def create
-    @job = Job.new(job_params)
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-    end
-
-  end
-
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
-    else
-      render :edit
-    end
-  end
-
   def show
     @job = Job.find(params[:id])
 
@@ -42,14 +19,35 @@ class JobsController < ApplicationController
     else
       Job.published.recent
     end
-
   end
-
-  def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-    redirect_to jobs_path
-  end
+  #
+  # def destroy
+  #   @job = Job.find(params[:id])
+  #   @job.destroy
+  #   redirect_to jobs_path
+  # end
+  #
+  # def create
+  #   @job = Job.new(job_params)
+  #   if @job.save
+  #     redirect_to jobs_path
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # def edit
+  #   @job = Job.find(params[:id])
+  # end
+  #
+  # def update
+  #   @job = Job.find(params[:id])
+  #   if @job.update(job_params)
+  #     redirect_to jobs_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   private
 
